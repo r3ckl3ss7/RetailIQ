a91938eaa8b872ead1a007eac81059e1c7206cfd
SarveshShahane <shahan77_soe@jnu.ac.in>
2026-05-31 20:39:52 +0530

    feat: include invoice router in the main application

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from sqlalchemy import text
 from db.database import engine
 from models.user import create_table
 from routes.auth import router as authRouter
+from routes.invoice import router as invoiceRouter
 from routes.user import router as userRouter
 app = FastAPI()
 
@@ -26,4 +27,5 @@ def db_health():
 
 
 app.include_router(authRouter)
+app.include_router(invoiceRouter)
 app.include_router(userRouter)
